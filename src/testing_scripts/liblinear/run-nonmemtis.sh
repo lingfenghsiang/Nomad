944f439470513e422c62124640f1f349fa9bd64c
lingfeng <lingfenghsiang@gmail.com>
2024-04-28 21:59:15 +0000

    liblinear benchmark scripts are ready for now

--- src/testing_scripts/liblinear/run-nonmemtis.sh
+++ src/testing_scripts/liblinear/run-nonmemtis.sh
@@ -9,6 +9,8 @@ function func_cache_flush() {
 function func_main() {
     
     TIME="/usr/bin/time"
+    rm /tmp/liblinear_initialized
+    rm /tmp/liblinear_thrashed
 
     # make directory for/run-tpp results-liblinear
     mkdir -p ${results_DIR}/results-liblinear
@@ -23,13 +25,22 @@ function func_main() {
     PID=$(ps | grep 'train' | head -n 1 | awk '{print $1}')
 
     echo $PID
-    # wait until page ranking finishes initialization
-    sleep 180
+
+    while [ ! -e "/tmp/liblinear_initialized" ]
+    do
+        sleep 1
+    done
     
     ${bin_DIR}/tpp_mem_access  -frun=${bin_DIR}/thrashing-15G.bin -anon \
                 -fwarmup=${bin_DIR}/thrashing-15G.bin --logtostderr
 
-    kill -SIGUSR1  $PID
+    echo please run > /tmp/liblinear_thrashed
+
+    PID=`pgrep train`
+    while [ -e /proc/$PID ]
+    do
+        sleep 1
+    done
 }
  
 
@@ -39,7 +50,7 @@ function func_main() {
 # thp_setting=madvise 
 source global_dirs.sh
 bin_DIR=${compiled_package_dir}
-results_DIR=${output_log_dir}/liblinear-nomad
+results_DIR=${output_log_dir}/liblinear-`uname -r`
 mkdir -p ${results_DIR}
 BENCH_BIN=third_party/tmp/liblinear-multicore-2.47
 BENCH_RUN="${BENCH_BIN}/train -s 6 -m 16 -e 0.000001  ${BENCH_BIN}/HIGGS"

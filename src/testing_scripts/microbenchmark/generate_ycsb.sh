45e83e0d9e0686ee8b11a964c2ef1276fb7499d8
lingfeng <lingfenghsiang@gmail.com>
2024-05-01 02:24:41 -0500

    added scripts to generate robustness access pattern file

--- src/testing_scripts/microbenchmark/generate_ycsb.sh
+++ src/testing_scripts/microbenchmark/generate_ycsb.sh
@@ -30,5 +30,13 @@ ${ycsb_dir}/bin/ycsb.sh run basic -P ${script_dir}/workloada-27G  | python3 ${sc
 offset=`${ycsb_dir}/bin/ycsb.sh run basic -P ${script_dir}/workloada-27G  | python3 ${script_dir}/filter_access_pattern.py | python3 ${script_dir}/write_binary_data.py -p 1 -f ${output_dir}/warmup_zipfan_first_touch_27G.bin`
 ${ycsb_dir}/bin/ycsb.sh run basic -P ${script_dir}/workloada-27G  | python3 ${script_dir}/filter_access_pattern.py | python3 ${script_dir}/write_binary_data.py -p 1 -o $offset -f ${output_dir}/run_zipfan_first_touch_27G.bin
 
+${ycsb_dir}/bin/ycsb.sh run basic -P ${script_dir}/robustness-23G  | python3 ${script_dir}/filter_access_pattern.py | python3 ${script_dir}/write_binary_data.py -p 1 -f ${output_dir}/robustness-23G.bin
+
+${ycsb_dir}/bin/ycsb.sh run basic -P ${script_dir}/robustness-25G  | python3 ${script_dir}/filter_access_pattern.py | python3 ${script_dir}/write_binary_data.py -p 1 -f ${output_dir}/robustness-25G.bin
+
+${ycsb_dir}/bin/ycsb.sh run basic -P ${script_dir}/robustness-27G  | python3 ${script_dir}/filter_access_pattern.py | python3 ${script_dir}/write_binary_data.py -p 1 -f ${output_dir}/robustness-27G.bin
+
+${ycsb_dir}/bin/ycsb.sh run basic -P ${script_dir}/robustness-29G  | python3 ${script_dir}/filter_access_pattern.py | python3 ${script_dir}/write_binary_data.py -p 1 -f ${output_dir}/robustness-29G.bin
+
 python3 ${script_dir}/create_thrashing.py -f ${output_dir}/thrashing-15G.bin -s 15
 python3 ${script_dir}/create_thrashing.py -f ${output_dir}/thrashing-10G.bin -s 10

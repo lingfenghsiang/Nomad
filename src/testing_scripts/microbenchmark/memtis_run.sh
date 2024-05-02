17d8d8d09446801277ef6b38cc59ecf4dce11b4c
lingfeng <lingfenghsiang@gmail.com>
2024-05-01 21:24:34 -0500

    fix typo, adjust sleep time between microbenchmark tests

--- src/testing_scripts/microbenchmark/memtis_run.sh
+++ src/testing_scripts/microbenchmark/memtis_run.sh
@@ -1,8 +1,12 @@
 bash src/testing_scripts/microbenchmark/memtis_commands/hottest_27G_read.sh
+sleep 20
 bash src/testing_scripts/microbenchmark/memtis_commands/hottest_27G_write.sh
-
+sleep 20
 bash src/testing_scripts/microbenchmark/memtis_commands/hottest_13.5G_read.sh
+sleep 20
 bash src/testing_scripts/microbenchmark/memtis_commands/hottest_13.5G_write.sh
-
+sleep 20
 bash src/testing_scripts/microbenchmark/memtis_commands/hottest_10G_read.sh
+sleep 20
 bash src/testing_scripts/microbenchmark/memtis_commands/hottest_10G_write.sh
+sleep 20

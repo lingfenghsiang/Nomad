a4728a8bf5bf5a43e602ca97a6476eccafc8429f
lingfeng <lingfenghsiang@gmail.com>
2024-05-03 03:01:03 +0000

    update tiering parameter

--- src/testing_scripts/setup_system/nomad_start_tiering.sh
+++ src/testing_scripts/setup_system/nomad_start_tiering.sh
@@ -6,5 +6,5 @@ cd src/nomad_module && make clean && make && insmod async_promote.ko
 echo 1 >/sys/kernel/mm/numa/demotion_enabled
 echo 2 >/proc/sys/kernel/numa_balancing
 swapoff -a
-echo 2000 >/proc/sys/vm/demote_scale_factor
+echo 2200 >/proc/sys/vm/demote_scale_factor
 cd ${original_dir}
--- src/testing_scripts/setup_system/tpp_start_tiering.sh
+++ src/testing_scripts/setup_system/tpp_start_tiering.sh
@@ -3,4 +3,4 @@
 echo 1 >/sys/kernel/mm/numa/demotion_enabled
 echo 2 >/proc/sys/kernel/numa_balancing
 swapoff -a
-echo 1000 >/proc/sys/vm/demote_scale_factor
+echo 2200 >/proc/sys/vm/demote_scale_factor

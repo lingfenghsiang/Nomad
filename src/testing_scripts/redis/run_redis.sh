116affc5c7561c0b157adbd904dd7d581ec708f9
lingfeng <lingfenghsiang@gmail.com>
2024-05-04 13:06:41 -0500

    update redis timeout

--- src/testing_scripts/redis/run_redis.sh
+++ src/testing_scripts/redis/run_redis.sh
@@ -5,12 +5,18 @@ sleep 15
 bash src/testing_scripts/redis/run_redis_client.large.sh
 sleep 60
 
+wait
+
 bash src/testing_scripts/redis/start_redis_server.sh &
 sleep 15
 bash src/testing_scripts/redis/run_redis_client.small.sh
 sleep 60
 
+wait
+
 bash src/testing_scripts/redis/start_redis_server.sh &
 sleep 15
 bash src/testing_scripts/redis/run_redis_client.noevict.sh
 sleep 60
+
+wait
